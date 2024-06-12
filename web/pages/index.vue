30dda05fdcf0e051b7898dcdb8c2061b5186c222
Shomari <nyusternie@sdot.io>
2024-06-12 10:34:22 -0400

    Enable real-time blockchain requests.

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
-import Nexa from 'nexajs'
+import { getTip } from '@nexajs/rostrum'
+import numeral from 'numeral'
+
+const blockHeight = ref(0)
+
+const init = async () => {
+    const currentTip = await getTip()
+    console.log('CURRENT TIP', currentTip)
+
+    blockHeight.value = numeral(currentTip.height).format('0,0')
+}
+
+onMounted(() => {
+    console.log('Mounted!')
+    init()
+})
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+
 </script>
 
 <template>
-    <main class="w-screen h-screen bg-gradient-to-r from-lime-200 to-lime-100">
+    <main class="w-screen h-screen flex flex-col bg-gradient-to-r from-lime-200 to-lime-100">
         <div class="h-full flex flex-col justify-center pb-32 items-center gap-5 sm:gap-8">
             <img src="~/assets/icon.png" class="w-24 sm:w-32 lg:w-40" />
 
@@ -14,6 +35,11 @@ import Nexa from 'nexajs'
             <h1 class="text-2xl sm:text-4xl lg:text-5xl text-lime-600 font-light italic">
                 Spend Privately. Fearlessly!
             </h1>
+
         </div>
+
+        <small class="py-1 pr-2 flex justify-end text-rose-500 text-xs font-medium italic opacity-50">
+            block # {{ blockHeight }}
+        </small>
     </main>
 </template>
