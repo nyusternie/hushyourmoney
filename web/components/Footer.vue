6b430abca82e555da73a7d83ad6ebd3748eb0f9d
Shomari <nyusternie@sdot.io>
2024-08-09 21:34:15 -0400

    Add new layout.

---
+++ web/components/Footer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+/* Define properties. */
+// https://vuejs.org/guide/components/props.html#props-declaration
+const props = defineProps({
+    data: {
+        type: [Object],
+    },
+})
+
+// onMounted(() => {
+//     console.log('Mounted!')
+//     // Now it's safe to perform setup operations.
+// })
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+</script>
+
+<template>
+    <footer>
+        <div class="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div class="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left"><span class="block sm:inline">&copy; 2021 Your Company, Inc.</span> <span class="block sm:inline">All rights reserved.</span></div>
+        </div>
+    </footer>
+</template>
--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,11 +1,4 @@
 <script setup lang="ts">
-useHead({
-    title: 'PROJECT_NAME — Nexa Studio',
-    meta: [
-        { name: 'description', content: 'Nexa Studio makes building your next BIG idea effortless.' }
-    ],
-})
-
 /* Initialize stores. */
 import { useProfileStore } from '@/stores/profile'
 import { useSystemStore } from '@/stores/system'
@@ -83,9 +76,57 @@ onMounted(() => {
 </script>
 
 <template>
-    <Header />
+    <div class="min-h-full">
+        <Header />
 
-    <div class="pt-32">
-        <slot />
+        <main class="-mt-24 pb-8">
+            <div class="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <h1 class="sr-only">
+                    Hush Your Money
+                </h1>
+
+                <!-- Main 3 column grid -->
+                <div class="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
+
+                    <!-- Left column -->
+                    <div class="grid grid-cols-1 gap-4 lg:col-span-2">
+                        <section aria-labelledby="section-1-title">
+                            <h2 class="sr-only" id="section-1-title">
+                                Dashboard
+                            </h2>
+
+                            <div class="overflow-hidden rounded-lg bg-white shadow">
+                                <div class="p-6">
+
+                                    <!-- Your content -->
+                                    <slot />
+
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+
+                    <!-- Right column -->
+                    <div class="grid grid-cols-1 gap-4">
+                        <section aria-labelledby="section-2-title">
+                            <h2 class="sr-only" id="section-2-title">
+                                Wealth Diary
+                            </h2>
+
+                            <div class="overflow-hidden rounded-lg bg-white shadow">
+                                <div class="p-6">
+
+                                    <!-- Your content -->
+                                    TBD...
+
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <Footer />
     </div>
 </template>
