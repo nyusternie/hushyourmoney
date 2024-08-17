41f436584c2c8d28b6c62d1070b143d160bf04c0
Shomari <nyusternie@sdot.io>
2024-08-17 12:34:52 -0400

    Update libs.

--- web/components/Readme/Intro.vue
+++ web/components/Readme/Intro.vue
@@ -25,7 +25,17 @@ const props = defineProps({
         </h2>
 
         <p>
-            Hush Your Money is a passion project, forged from years of frustration from ALL the broken promises.
+            <span class="text-lg font-bold">Hush Your Money (<span class="text-xl">HYM</span>)</span> is a project of pure passion; forged after years of frustration from ALL the broken promises of "fungible" crypto payments.
         </p>
+
+        <h3 class="font-bold">
+            Choose 1 of 3 convenient methods to Hush Your Money:
+        </h3>
+
+        <ol class="-mt-3 pl-10 list-decimal leading-[50px]">
+            <li>Privatize your assets ahead of time using ANY HYM/Club pool.</li>
+            <li>Transfer ANY asset (in real-time) directly from ANY HYM/Club pool.</li>
+            <li>Transfer NITO assets to anyone privately via ANY <NuxtLink to="https://nexa.wiki/supernet" target="_blank" class="text-blue-500 font-bold hover:underline">Incognito Supernet</NuxtLink>.</li>
+        </ol>
     </main>
 </template>
